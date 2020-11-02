5a8bbee3ea44886724dd411619430fe7f2e2625b
James Wagoner <jameswagoner@gmail.com>
2020-11-02 13:05:23 -0800

    Unit test setup

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WAGoner\ZoomWrapper;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
